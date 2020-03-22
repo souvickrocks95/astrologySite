b48d62e07d8a31376f486f11b39f4cc4d391eace
souvickrocks95 <souvickrocks95@gmail.com>
2020-03-22 17:23:57 +0530

    Souvick

--- src/app/app-hover.directive.ts
+++ src/app/app-hover.directive.ts
@@ -13,7 +13,6 @@ export class AppHoverDirective {
 
   @HostListener('mouseover') onMouseOvr(){
    this.rndr.setElementStyle(this.elmnt.nativeElement,'backgroundColor',' #003366');
-  // this.rndr.setElementStyle(this.elmnt.nativeElement,'color','black');
    this.rndr.setElementStyle(this.elmnt.nativeElement,'border-color',' rgba(0, 0, 0, 0.7)');
 
 }
@@ -21,7 +20,6 @@ export class AppHoverDirective {
 @HostListener('mouseout') onMouseOut(){
  this.rndr.setElementStyle(this.elmnt.nativeElement,'background-color','Transparent');
  this.rndr.setElementStyle(this.elmnt.nativeElement,'background-repeat','no-repeat');
- this.rndr.setElementStyle(this.elmnt.nativeElement,'color','#fff');
  this.rndr.setElementStyle(this.elmnt.nativeElement,'border-color','#ccc');
 }
 
